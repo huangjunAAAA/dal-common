11dec1a99d005f43d106893f91c4fa5c6e4ab12c
huangjun <huangjun@jiangtai.com>
2020-10-25 01:57:27 +0800

    done reading property.

--- rpc-client/src/main/java/com/boring/dal/remote/autorpc/RpcClientRegistry.java
+++ rpc-client/src/main/java/com/boring/dal/remote/autorpc/RpcClientRegistry.java
@@ -9,21 +9,28 @@ import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
 
 @Component
-public class RpcClientRegistry implements BeanDefinitionRegistryPostProcessor {
+public class RpcClientRegistry implements BeanDefinitionRegistryPostProcessor, ApplicationContextAware {
 
     private String clientPkg="com.rpc.client";
 
-    Set<Class<?>> beanClazzs;
+    private ApplicationContext applicationContext;
+
     @Override
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        String clientPkg=readProperty((ConfigurableEnvironment) applicationContext.getEnvironment(),"client.package");
         Reflections reflections = new Reflections(clientPkg, new TypeAnnotationsScanner(), new SubTypesScanner());
 
-        beanClazzs = reflections.getTypesAnnotatedWith(AutoRpc.class);
+        Set<Class<?>> beanClazzs = reflections.getTypesAnnotatedWith(AutoRpc.class);
         for (Class beanClazz : beanClazzs) {
             BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(beanClazz);
             GenericBeanDefinition definition = (GenericBeanDefinition) builder.getRawBeanDefinition();
@@ -39,5 +46,25 @@ public class RpcClientRegistry implements BeanDefinitionRegistryPostProcessor {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
 
+    }
+
+
+    private String readProperty(ConfigurableEnvironment environment, String pkey) {
+        for (PropertySource<?> source : environment.getPropertySources()) {
+            if (source instanceof EnumerablePropertySource) {
+                EnumerablePropertySource<?> propertySource = (EnumerablePropertySource<?>) source;
+                for (String property : propertySource.getPropertyNames()) {
+                    if (pkey.equals(property))
+                    {
+                        return (String)propertySource.getProperty(pkey);
+                    }
+                }
+            }
+        }
+        throw new IllegalStateException("Unable to determine value of property " + pkey);
+    }
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext=applicationContext;
     }
 }
